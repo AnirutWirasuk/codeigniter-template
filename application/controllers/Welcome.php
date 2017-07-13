b6ef37ad30d6c55b78e0b7a624b7f9536aefaee7
Anirut <a.w.wirasuk@hotmail.com>
2017-07-13 14:01:16 +0700

    Add libraries template

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,24 +2,18 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
+	function __construct(){
+		parent::__construct();
+		$this->load->library('template');
+	}
+	public function index(){
+		$data = array();
+		$this->template->css(array(
+				"assets/css/style"
+			));
+		$this->template->js(array(
+				"assets/css/javascript"
+			));
+		$this->template->backend("welcome_message",$data);
 	}
 }
---
+++ application/views/themes/template_backmain.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE" />
+    <title>System Administrator VistaWiFi - v1.0</title>
+
+    <?=$css;?>
+</head>
+<body >
+
+    <?=$contents;?>
+
+    <?=$js;?>
+    
+</body>
+</html>
