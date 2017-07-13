29be496cafc8d329738448c74530c2e6c94f088c
Anirut <a.w.wirasuk@hotmail.com>
2017-07-13 13:42:58 +0700

    Start project

---
+++ application/modules/home/controllers/device.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Device extends CI_Controller {
+
+	public function index()
+	{
+		die('device');
+	}
+	public function adddevice(){
+		die("add-device");
+	}
+}
